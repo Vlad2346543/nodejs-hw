d4fd2377c9710b0e80a13bc31ac03548ebae129a
Vlad2346543 <manxeb22@gmail.com>
2026-04-05 16:47:53 +0300

    fix: add pino-pretty and update gitignore

--- src/server.js
+++ src/server.js
@@ -14,7 +14,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(pinoHttp());
+app.use(
+  pinoHttp({
+    transport: {
+      target: 'pino-pretty',
+    },
+  })
+);
 
 app.get('/notes/:nodeId', (req, res) => {
     const {noteId} = req.params;
