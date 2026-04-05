e4943552dfb3c13a1c0dd164f7126aea383fcc27
Vlad2346543 <manxeb22@gmail.com>
2026-04-05 17:01:57 +0300

    fix: use next() for error handling

--- src/server.js
+++ src/server.js
@@ -36,8 +36,8 @@ app.get('/notes/:noteId', (req, res) => {
 });
 
 
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
+app.get('/test-error', (req, res, next) => {
+  next(new Error('Simulated server error'));
 });
 
 
