e3b8cd6dbe7bf09699300abf3615f2db70faa7dc
Vlad2346543 <manxeb22@gmail.com>
2026-04-12 03:13:43 +0300

    fix auth validation password rules

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,15 +1,17 @@
 import { Joi, Segments } from "celebrate";
 
+//  REGISTER
 export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+    password: Joi.string().min(8).required(), // 🔥 8
   }),
 };
 
+//  LOGIN
 export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
+    password: Joi.string().required(), // 🔥 БЕЗ min
   }),
 };
